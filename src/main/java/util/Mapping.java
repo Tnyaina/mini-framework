cf7852397fb77e0aedd4c1bd11468fb303597493
Tnyaina <tojoandriam238@gmail.com>
2025-11-07 15:03:15 +0300

    Sprint 4
    invoke printwritter la methode

--- src/main/java/util/Mapping.java
+++ src/main/java/util/Mapping.java
@@ -1,6 +1,12 @@
 package util;
 
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import annotation.Get;
+import annotation.Post;
 
 public class Mapping {
     private Class<?> controllerClass;
@@ -25,5 +31,31 @@ public class Mapping {
     
     public void setMethod(Method method) {
         this.method = method;
+    }
+
+    public static Map<String, Mapping> scanControllers() throws Exception {
+        Map<String, Mapping> urlMappings = new HashMap<>();
+        
+        List<Class<?>> controllers = ControllerScanner.getAllControllers();
+        System.out.println("Controleurs trouves: " + controllers.size());
+        
+        for (Class<?> controller : controllers) {
+            System.out.println("Controleur: " + controller.getName());
+            for (Method method : controller.getDeclaredMethods()) {
+                if (method.isAnnotationPresent(Get.class)) {
+                    String url = method.getAnnotation(Get.class).value();
+                    System.out.println("  GET " + url + " -> " + method.getName());
+                    urlMappings.put("GET:" + url, new Mapping(controller, method));
+                }
+                if (method.isAnnotationPresent(Post.class)) {
+                    String url = method.getAnnotation(Post.class).value();
+                    System.out.println("  POST " + url + " -> " + method.getName());
+                    urlMappings.put("POST:" + url, new Mapping(controller, method));
+                }
+            }
+        }
+        System.out.println("Total mappings: " + urlMappings.size());
+        
+        return urlMappings;
     }
 }
